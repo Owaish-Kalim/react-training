33de42f7ae039a32513783bf16cb20bb9cb85ed1
Owaish Kalim <owaishkalim@gmail.com>
2021-05-17 21:36:43 +0530

    Multi comp

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
@@ -12,4 +13,4 @@ function ExpenseItem(props) {
   );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
